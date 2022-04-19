df3042b91e218aa471ff06c8e1cdd717f01e2bb5
Andres Perez <aperez@salesforce.com>
2022-04-19 12:24:19 -0400

    Step_3.3 - LWC - Load and display bears (imperative Apex)

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,4 +1,23 @@
 import { LightningElement } from 'lwc';
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+/** BearController.getAllBears() Apex method */
+import getAllBears from '@salesforce/apex/BearController.getAllBears';
 export default class BearList extends LightningElement {
-
+    bears;
+    error;
+    appResources = {
+        bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
+    };
+    connectedCallback() {
+        this.loadBears();
+    }
+    loadBears() {
+        getAllBears()
+            .then(result => {
+                this.bears = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
 }
