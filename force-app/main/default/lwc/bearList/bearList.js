befa7698c123c34808ee280a2df0791502d6fddd
Andres Perez <aperez@salesforce.com>
2022-04-19 12:24:20 -0400

    Step_3.5 - LWC - Code to load and display bears (wired) [**--VALIDATE--**]

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,23 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.getAllBears() Apex method */
 import getAllBears from '@salesforce/apex/BearController.getAllBears';
 export default class BearList extends LightningElement {
-    bears;
-    error;
+    @wire(getAllBears) bears;
     appResources = {
         bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
     };
-    connectedCallback() {
-        this.loadBears();
-    }
-    loadBears() {
-        getAllBears()
-            .then(result => {
-                this.bears = result;
-            })
-            .catch(error => {
-                this.error = error;
-            });
-    }
 }
