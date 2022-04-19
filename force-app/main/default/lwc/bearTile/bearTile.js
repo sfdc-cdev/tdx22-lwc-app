a44763e7a8c8aa91af7052585454afefd641f709
Andres Perez <aperez@salesforce.com>
2022-04-19 12:24:21 -0400

    Step_4.4 - LWC - Dispatch ”bearview” event from child (bearTile)

--- force-app/main/default/lwc/bearTile/bearTile.js
+++ force-app/main/default/lwc/bearTile/bearTile.js
@@ -6,6 +6,9 @@ export default class BearTile extends LightningElement {
         bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
     };
     handleOpenRecordClick() {
-
+        const selectEvent = new CustomEvent('bearview', {
+            detail: this.bear.Id
+        });
+        this.dispatchEvent(selectEvent);
     }
 }
