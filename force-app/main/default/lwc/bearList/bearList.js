f1246112ec5ebbd469fc755b744d2c29f23e1db9
Andres Perez <aperez@salesforce.com>
2022-04-19 12:24:21 -0400

    Step_5.2 - LWC - Publishes LMS when bears are loaded

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,11 +1,23 @@
+import { publish, MessageContext } from 'lightning/messageService';
+import BEAR_LIST_UPDATE_MESSAGE from '@salesforce/messageChannel/BearListUpdate__c';
 import { NavigationMixin } from 'lightning/navigation';
 import { LightningElement, wire } from 'lwc';
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearList extends NavigationMixin(LightningElement) {
     searchTerm = '';
-    @wire(searchBears, { searchTerm: '$searchTerm' })
     bears;
+    @wire(MessageContext) messageContext;
+    @wire(searchBears, { searchTerm: '$searchTerm' })
+    loadBears(result) {
+        this.bears = result;
+        if (result.data) {
+            const message = {
+                bears: result.data
+            };
+            publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+        }
+    }
     handleSearchTermChange(event) {
         // Debouncing this method: do not update the reactive property as
         // long as this function is being called within a delay of 300 ms.
