8c36b22ce3a911e3d7d92eca28e6fa69e35d2ae1
Andres Perez <aperez@salesforce.com>
2022-04-19 12:24:17 -0400

    Step_1.6 - LWC - Enable input for greeting property

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,4 +1,7 @@
 import { LightningElement } from 'lwc';
 export default class HelloWebComponent extends LightningElement {
     greeting = 'Trailblazer';
+    handleGreetingChange(event) {
+        this.greeting = event.target.value;
+    }
 }
